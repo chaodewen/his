b7e76b51f1cdb01cd3b9bfe19255ae3f7898fbde
Cc <vaal.just@qq.com>
2016-12-26 16:31:47 +0800

    small changes

--- src/main/java/zju/ccnt/mdsp/controller/RecipeService.java
+++ src/main/java/zju/ccnt/mdsp/controller/RecipeService.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import zju.ccnt.mdsp.db.MySqlSessionFactory;
+import zju.ccnt.mdsp.model.DrugItem;
 import zju.ccnt.mdsp.model.Recipe;
 import zju.ccnt.mdsp.utils.Utils;
 
